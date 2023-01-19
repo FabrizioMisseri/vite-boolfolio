404094a8aaa814d6b3b99605bf1c77a32b5d23c1
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-19 15:49:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'app',
+
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from "axios";
+
+export default {
+    name: 'AppMain',
+
+    data() {
+        return {
+            apiUrlProjects: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+
+    created() {
+        this.getProjects();
+    },
+
+    methods: {
+        getProjects() {
+            axios.get(this.apiUrlProjects).then(resp => {
+                // console.log(resp);
+                const data = resp.data.results;
+                return data;
+            })
+        },
+    }
+}
+</script>
+
+<template>
+
+    ciao
+</template>
+
+<style lang="scss" scoped>
+
+</style>
